e73a5c4165f6d217afc9fc4392913c0bce98dac9
Andre Wibbeke <andre.wibbeke@gmail.com>
2020-02-03 14:35:28 +0100

    feat: create blog posts dynamically

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,3 +13,41 @@ module.exports.onCreateNode = ({ node, actions }) => {
     })
   }
 }
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  // Get path to template
+  const blogTemplate = path.resolve(`./src/templates/blog.js`)
+
+  // Query for markdown nodes to use in creating pages
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    throw result.errors
+  }
+
+  // Create blog post pages
+  result.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      // Path for this page — required
+      path: `/blog/${edge.node.fields.slug}`,
+      component: blogTemplate,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
+}
