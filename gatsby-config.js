4fae2f64f5bafac89f565d046d89611d630ec6b9
Andre Wibbeke <andre.wibbeke@gmail.com>
2020-02-03 15:18:57 +0100

    feat: enable Markdown syntax for adding images inline

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,19 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    "gatsby-transformer-remark",
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 750,
+            },
+          },
+        ],
+      },
+    },
   ],
 }
