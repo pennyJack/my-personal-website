9e7155de3779445d5d378f0c4d576593f3809f26
Andre Wibbeke <andre.wibbeke@gmail.com>
2020-02-23 13:45:18 +0100

    design: use Github typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,6 @@
 import Typography from "typography"
+import githubTheme from "typography-theme-github"
 
-const typography = new Typography({
-  baseFontSize: "18px",
-  baseLineHeight: 1.666,
-  headerFontFamily: [
-    "Avenir Next",
-    "Helvetica Neue",
-    "Segoe UI",
-    "Helvetica",
-    "Arial",
-    "sans-serif",
-  ],
-  bodyFontFamily: ["Georgia", "serif"],
-})
+const typography = new Typography(githubTheme)
 
 export default typography
